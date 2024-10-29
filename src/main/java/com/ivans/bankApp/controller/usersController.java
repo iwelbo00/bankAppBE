21f5960474d70af18541c0cd455c72e741a5961f
ivan welborn <iwelbo00@uafs.edu>
2024-10-28 19:48:18 -0500

    email sending functionality

--- src/main/java/com/ivans/bankApp/controller/usersController.java
+++ src/main/java/com/ivans/bankApp/controller/usersController.java
@@ -1,17 +1,19 @@
 package com.ivans.bankApp.controller;
 
+import com.ivans.bankApp.entity.emailDetails;
 import com.ivans.bankApp.entity.userDetails;
 import com.ivans.bankApp.repository.usersRepository;
 import com.ivans.bankApp.services.userService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin("http://localhost:8081/")
-
+@CrossOrigin("http://localhost:5173")
 public class usersController {
     private com.ivans.bankApp.services.userService userService;
 
@@ -21,6 +23,8 @@ public class usersController {
     public void userController(userService userService) {
         this.userService = userService;
     }
+    @Autowired
+    private JavaMailSender mailSender;
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody userDetails request){
@@ -30,6 +34,24 @@ public class usersController {
             return ResponseEntity.ok("Authorization Successful");
         }else{
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed");
+        }
+    }
+
+    @PostMapping("/sendEmail")
+    public void sendEmail(@RequestBody emailDetails request){
+        sendEmail(request.getSendTo(), request.getSubject(), request.getBody());
+    }
+
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        for(int i = 0; i < 100; i++) {
+            mailSender.send(message);
+            message.setText(body+i);
         }
     }
 
---
+++ src/main/java/com/ivans/bankApp/entity/emailDetails.java
@@ -0,0 +1,13 @@
+package com.ivans.bankApp.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class emailDetails {
+
+    private String subject;
+    private String sendTo;
+    private String body;
+}
